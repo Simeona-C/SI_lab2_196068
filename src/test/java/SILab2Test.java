1cfb436c4b32b959d8f2c13be094078d3717d17a
Stefan Ivanovski <stefs.ivanovski@tuta.io>
2021-05-17 18:23:06 +0200

    Initial

---
+++ src/test/java/SILab2Test.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SILab2Test {
+
+    @Test
+    void function_MultipleCondition(List<Time> timesList) {
+        
+    }
+
+    @Test
+    void function_EveryBranch(List<Time> timesList) {
+
+    }
+}
